1acb8f35037eeb94732cf6ac63075430bc097557
Diego Luna Granados <slowkingvii@gmail.com>
2020-02-18 23:25:02 -0600

    Add class Guess which evaluates user input

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,7 +1,21 @@
+RXP = /(\d)[^\d]*(\d)[^\d]*(\d)[^\d]*(\d)[^\d]*/.freeze
+
 class Board
   def initialize(slots)
     @slots = slots
     @code = []
     4.times { @code.push(rand(1..4)) }
+  end
+end
+
+class Guess
+  def initialize(inp)
+    @code = rxp_eval(inp)
+  end
+
+  private
+
+  def rxp_eval(txt)
+    RXP.match(txt).captures
   end
 end
