e4a13beb1648a17e73946a6fa18e2a7ea0131eaf
Diego Luna Granados <slowkingvii@gmail.com>
2020-02-18 20:20:21 -0600

    Initialize user interface as a placeholder

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,22 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
-puts 'How\'re you doin\'?'
-puts 'MASTERMIND'
+puts 'Welcome to MasterMind!
+
+Press [Enter] to continue...'
+gets
+
+print 'Code Generated! [ ? ? ? ? ] Enter your first guess: '
+inp = gets.strip.split('')
+puts "| #{inp[0]} #{inp[1]} #{inp[2]} #{inp[3]} | >> Feedback #1: 0OXX"
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+puts '|         |'
+
+puts 'Congratulations! You have guessed!'
